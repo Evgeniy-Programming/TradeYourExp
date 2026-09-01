e91feb7a73bf1c43d2c0307c7558c6dac3772893
Иосипчук Максим <masimiosipcuk2020@gmail.com>
2026-09-01 21:54:11 +0300

    feat: add RTK store

---
+++ frontend/src/store/slices/profileSlice.ts
@@ -0,0 +1,39 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { IProfile } from "../../types/profile";
+
+interface ProfileState {
+    profile: IProfile | null;
+    isLoading: boolean;
+}
+
+const initialState: ProfileState = {
+    profile: null,
+    isLoading: false
+};
+
+const profileSlice = createSlice({
+    name: 'profile',
+    initialState,
+    reducers: {
+        setProfile: (state, action: PayloadAction<IProfile>) => {
+            state.profile = action.payload;
+        },
+        updateProfileFields: (state, action: PayloadAction<Partial<IProfile>>) => {
+            if (state.profile) {
+                state.profile = {
+                    ...state.profile,
+                    ...action.payload
+                }
+            }
+        },
+        clearProfile: (state) => {
+            state.profile = null;
+        },
+        setProfileLoading: (state, action: PayloadAction<boolean>) => {
+            state.isLoading = action.payload;
+        }
+    }
+});
+
+export const { setProfile, updateProfileFields, clearProfile, setProfileLoading } = profileSlice.actions;
+export default profileSlice.reducer;
---
+++ frontend/src/types/profile.ts
@@ -0,0 +1,8 @@
+export interface IProfile {
+    id: string;
+    username: string;
+    email: string;
+    firstName: string;
+    lastName: string;
+    createdAt: Date;
+}
